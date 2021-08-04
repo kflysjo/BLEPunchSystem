ccfbef9a9a1b3287826d502e8ef987a51d9825fc
kflysjo <37731803+kflysjo@users.noreply.github.com>
2021-08-04 19:50:47 +0200

    Update PunchCentralSDlog.ino

--- PunchCentralSDlog.ino
+++ PunchCentralSDlog.ino
@@ -1,13 +1,25 @@
 #include <Arduino.h>
 #include <ArduinoBLE.h>
+#include <SPI.h>
+#include <SD.h>
+#include <string.h>
+
+
+File myFile;
+long random_number;
+char filename_buffer[16];
+char rng_buffer[8];
+char individual_log_appendage[] = ".csv";
 
 const char* uuidOfRxServiceLH = "00002A3D-0000-1000-8000-00805f9b34fb";
 const char* uuidOfRxCharacteristicLH = "00012A3D-0000-1000-8000-00805f9b34fb";
 
 const int RX_BUFFER_SIZE = 200;
+const int RX_TIMEOUT = 200;
 char RX_BUFFER[RX_BUFFER_SIZE];
 bool RX_BUFFER_FIXED_LENGTH = true;
 int succesful_rx_counter = 0;
+long int current_millis, last_succesful_rx_millis;
 
 void connectToPeripheral();
 void controlPeripheral(BLEDevice peripheralLH);
@@ -20,6 +32,47 @@ void setup() {
   Serial.begin(115200);
   while (!Serial);
 
+  randomSeed(analogRead(A0));
+
+
+  Serial.print("Initializing SD card...");
+  pinMode(9, OUTPUT);
+  delay(1000);
+
+  while (!SD.begin(9)) 
+  {
+    Serial.println("SD-card initialization failed!");
+    delay(1000);
+  }
+  Serial.println("SD-card initialization done.");
+
+    for (int i = 0; i < 3; i++)
+  {
+
+    random_number = random(10,100);
+    Serial.print("random number: ");
+    Serial.println(random_number);
+
+    sprintf(rng_buffer, "%ld", random_number);
+    Serial.print("strlen of rng buffer: ");
+    Serial.println(strlen(rng_buffer));
+
+    strcat(filename_buffer, rng_buffer);
+    Serial.print("strlen of filenamebuffer: ");
+    Serial.println(strlen(filename_buffer));
+
+  }
+
+  strcat(filename_buffer, individual_log_appendage);
+
+  Serial.print("Full filename: ");
+  Serial.println(filename_buffer);
+
+  Serial.print("strlen of filename: ");
+  Serial.println(strlen(filename_buffer));
+
+
+  
   if (!BLE.begin()) {
     Serial.println("* Starting BLE module failed!");
     while (1);
@@ -63,6 +116,7 @@ void connectToPeripheral(){
 }
 
 void controlPeripheral(BLEDevice peripheralLH) {
+  
   Serial.println("- Connecting to peripheralLH device...");
   if (peripheralLH.connect()) {
     Serial.println("* Connected to peripheralLH device!");
@@ -107,23 +161,45 @@ void controlPeripheral(BLEDevice peripheralLH) {
     return;
   }
  
-
-  while (peripheralLH.connected()) {
-    if (simpleKeyCharacteristicLH.valueUpdated()) {
-      simpleKeyCharacteristicLH.readValue(RX_BUFFER, RX_BUFFER_SIZE);
-      succesful_rx_counter++;
-      
-      delayMicroseconds(1);
-
-    if (succesful_rx_counter % 1000 == 0)
-    {
-      Serial.println(millis());
-      Serial.println(succesful_rx_counter);
-      Serial.println(RX_BUFFER);
-    }
-    
-
-  }
   
-}
+  delay(100);
+  myFile = SD.open(filename_buffer, FILE_WRITE);
+  delay(100);
+  myFile.println("SD-card object has been created");
+
+  myFile.println("File is open, glhf");
+
+  last_succesful_rx_millis = millis();
+  current_millis = millis();
+  
+  while (peripheralLH.connected()/* && (current_millis - last_succesful_rx_millis) > RX_TIMEOUT*/) {
+    current_millis = millis();
+    if (simpleKeyCharacteristicLH.valueUpdated()) {
+      
+      last_succesful_rx_millis = millis();
+      simpleKeyCharacteristicLH.readValue(RX_BUFFER, RX_BUFFER_SIZE);
+      delayMicroseconds(500);
+      
+      //myFile.print(millis());
+      //myFile.print(", ");
+      //myFile.print(RX_BUFFER);
+      
+      myFile.println("apa");
+      myFile.flush();
+      
+      delayMicroseconds(500);
+      //Serial.println(RX_BUFFER);
+  }
+
+  
+    if((current_millis - last_succesful_rx_millis) > RX_TIMEOUT){
+      Serial.println("RX timeout");      
+      break;
+    }
+  
+  }
+
+  Serial.println("Disconnected from LH");
+  myFile.close();
+
 }
